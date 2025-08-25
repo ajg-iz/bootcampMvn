f429b04c47f40f06772bfca067fc9da02aedb526
ajg-iz <andreas.goebel@gmail.com>
2025-08-25 15:17:29 +0200

    added playFizzBuzz(int number)

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,10 +1,30 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.example.Main.isEven;
+import static org.example.Main.playFizzBuzz;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @org.junit.jupiter.api.Test
-    void isEven() {
+    void isEven_shouldReturnTrue_withInput2() {
+        assertTrue(isEven(2));
+    }
+
+    @Test
+    void playFizzBuzz_shouldReturnFizz_ifNumberIsDividableBy3() {
+        assertEquals("Fizz",playFizzBuzz(99));
+    }
+
+    @Test
+    void playFizzBuzz_shouldReturnBuzz_ifNumberIsDividableBy5() {
+        assertEquals("Buzz",playFizzBuzz(25));
+    }
+
+    @Test
+    void playFizzBuzz_shouldReturnFizzBuzz_ifNumberIsDividableBy3And5() {
+        assertEquals("Fizzbuzz",playFizzBuzz(15));
     }
 }
